ce2912d3c7ad7a3e4e643aadc62c12e440526999
e-武士 <chris@aluna.io>
2023-06-11 20:36:15 -0400

    Initial commit for never-ending-story.swift ```

---
+++ never-ending-story.swift
@@ -0,0 +1,12 @@
+/**
+    - Kuza never-ending story 6/10/2021
+
+In the land of Kemet, there lived a young girl named Nefetari. 
+She was a skilled artisan and spent her days creating beautiful pottery that adorned 
+the homes of the wealthy and powerful. But Nefetati yearned for something more than just making pots; 
+she wanted adventure and excitement in her life. One day, as she was walking through the bustling marketplace, 
+she overheard a group of merchants discussing a valuable item they had acquired from a recently discovered tomb.
+
+
+
+**/
